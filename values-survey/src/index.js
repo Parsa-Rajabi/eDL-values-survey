3f08c05a6ad9661a77a53519328a363d991977f1
Parsa <ParsaRajabiPR@gmail.com>
2021-08-16 15:41:04 -0700

    Replaced App.js with Survey

--- values-survey/src/index.js
+++ values-survey/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Survey from './Survey';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Survey />
   </React.StrictMode>,
   document.getElementById('root')
 );
